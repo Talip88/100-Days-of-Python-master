d2b91f7fa9c7423f31789335f1f4012f4a2edcce
talip <talipclk1988@gmail.com>
2023-08-10 12:01:12 -0500

    Initial commit

---
+++ Dersler/Fonksyonlar.py
@@ -0,0 +1,7 @@
+#Parametresiz fonksyon##
+def my_function():
+    print("ilk fonksyon")
+my_function()
+    # indent ya space ile ya da tab ile 4 girinti içeri girilmesi ve fonksyonun scope unun belirlenmesi için yazılır.
+
+
